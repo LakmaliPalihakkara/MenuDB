5069206542b83c6e27c56c4e2feb9bd361b8bd53
binduvelivela <bindu.velivela0202@gmail.com>
2022-08-03 22:58:06 -0400

    Added display menu activity

--- app/src/main/java/com/example/sqlitedbproj/DBHelper.java
+++ app/src/main/java/com/example/sqlitedbproj/DBHelper.java
@@ -2,8 +2,12 @@ package com.example.sqlitedbproj;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -58,4 +62,39 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+
+public ArrayList<DataModel> getMenu(SQLiteDatabase db)
+{
+    ArrayList<DataModel> dataModalArrayList = new ArrayList<>();
+    // Define a projection that specifies which columns from the database
+    // you will actually use after this query.
+    String[] projection = {ID_COL,NAME_COL,DESCRIPTION_COL,PRICE_COL,CALORIES_COL};
+    // Filter results WHERE "title" = 'My Title'
+    String selection = " = ?";;
+    String[] selectionArgs = { "*" };
+
+    // How you want the results sorted in the resulting Cursor
+    String sortOrder =
+           ID_COL + " DESC";
+    Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    String str="";
+    while(cursor.moveToNext()) {
+        String id = cursor.getString(
+                cursor.getColumnIndexOrThrow(ID_COL));
+        String name = cursor.getString(
+                cursor.getColumnIndexOrThrow(NAME_COL));
+        String description = cursor.getString(
+                cursor.getColumnIndexOrThrow(DESCRIPTION_COL));
+        String calories = cursor.getString(
+                cursor.getColumnIndexOrThrow(CALORIES_COL));
+        String price = cursor.getString(
+                cursor.getColumnIndexOrThrow(PRICE_COL));
+        str = str + " " + id + " " + description;
+        DataModel datamodel = new DataModel(name,description,price,calories,id);
+        dataModalArrayList.add(datamodel);
+    }
+    cursor.close();
+    return dataModalArrayList;
+}
+
 }
---
+++ app/src/main/java/com/example/sqlitedbproj/DisplayMenu.java
@@ -0,0 +1,33 @@
+package com.example.sqlitedbproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class DisplayMenu extends AppCompatActivity {
+    GridView menu_grid_view;
+    ArrayList<DataModel> dataModalArrayList;
+    private DBHelper dbHelper;
+    SQLiteDatabase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_menu);
+        dbHelper = new DBHelper(DisplayMenu.this);
+        db = dbHelper.getReadableDatabase();
+        menu_grid_view = findViewById(R.id.menu_grid_view);
+        loadMenu();
+        Toast.makeText(DisplayMenu.this, "data loaded ", Toast.LENGTH_SHORT).show();
+    }
+    private void loadMenu()
+    {
+        dataModalArrayList = dbHelper.getMenu(db);
+        DisplayMenuAdapter adapter = new DisplayMenuAdapter(DisplayMenu.this, dataModalArrayList);
+        menu_grid_view.setAdapter(adapter);
+    }
+}
